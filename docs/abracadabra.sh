f2084a4b572aa975b9f71ddd6be0f50d567d11ca
Asanka Herath <asanka@google.com>
2020-05-30 15:22:55 -0400

    home button

--- docs/abracadabra.sh
+++ docs/abracadabra.sh
@@ -5,6 +5,6 @@ pandoc  --no-highlight \
         --lua-filter=task-list.lua \
         --from       markdown_github+smart+yaml_metadata_block+auto_identifiers \
         --to         html5 \
-        --template   ./cayman.template \
+        --template   ../../asankah.github.io/docs/article.template \
         --output     ./index.html \
         ../README.md ./configuration.yaml
